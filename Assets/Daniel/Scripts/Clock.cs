a352e61bb11eadac43b1298624ae1ef6ab454d0d
Daniel667Liu <92137425+Daniel667Liu@users.noreply.github.com>
2022-10-12 17:47:27 -0400

    clock 1.0

---
+++ Assets/Daniel/Scripts/Clock.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    public GameObject hourHand;
+    public GameObject minuteHand;
+    bool canControl = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        ClockRun();
+        AutoRotate();
+    }
+    void ClockRun() 
+    {
+
+        
+        if (canControl) 
+        {
+            Rigidbody hourRB = hourHand.GetComponent<Rigidbody>();
+            Rigidbody minuteRB = minuteHand.GetComponent<Rigidbody>();
+            hourRB.AddRelativeTorque(new Vector3(0, 0, 8f*Input.mouseScrollDelta.y*(Mathf.Abs(hourRB.angularVelocity.z)  + 0.02f)), ForceMode.Force);
+            minuteRB.AddRelativeTorque(new Vector3(0, 0, 8f * Input.mouseScrollDelta.y * (Mathf.Abs(minuteRB.angularVelocity.z)  + 0.02f)), ForceMode.Force);
+        }
+        if (Mathf.Abs(minuteHand.GetComponent<Rigidbody>().angularVelocity.z) >= 6) 
+        {
+            canControl = false;
+        }
+        
+
+    }
+
+   
+
+    void AutoRotate() 
+    {
+        if (!canControl) 
+        {
+            Rigidbody hourRB = hourHand.GetComponent<Rigidbody>();
+            Rigidbody minuteRB = minuteHand.GetComponent<Rigidbody>();
+            hourRB.angularVelocity = new Vector3(0, 0, -0.4f);
+            minuteRB.angularVelocity = new Vector3(0, 0, -6.1f);
+        }
+        
+        
+    }
+}
