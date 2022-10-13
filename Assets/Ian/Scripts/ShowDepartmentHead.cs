7cd05685d043fa01027c109deceb59070b3958cd
Daniel667Liu <92137425+Daniel667Liu@users.noreply.github.com>
2022-10-13 15:11:37 -0400

    midterm playtest

---
+++ Assets/Ian/Scripts/ShowDepartmentHead.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowDepartmentHead : MonoBehaviour
+{
+    public Animator departmentHead;
+
+    public void MoveDepartmentHead()
+    {
+        departmentHead.enabled = true;
+    }
+}
