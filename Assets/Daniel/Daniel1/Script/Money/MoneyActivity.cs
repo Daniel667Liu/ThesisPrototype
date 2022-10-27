2014e857f63cd0eb8fef4a0b133aa7d840993853
Daniel667Liu <92137425+Daniel667Liu@users.noreply.github.com>
2022-10-27 14:37:21 -0400

    1027 Non-band interaction added

--- Assets/Daniel/Daniel1/Script/Money/MoneyActivity.cs
+++ Assets/Daniel/Daniel1/Script/Money/MoneyActivity.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class MoneyActivity : MonoBehaviour
 {
+    Animator animator;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (TryGetComponent<Animator>(out animator))
+        {
+        }
+        else
+        {
+            Debug.Log("no animator attached to money");
+        }
+
+        if (TryGetComponent<AudioSource>(out audioSource))
+        {
+        }
+        else
+        {
+            Debug.Log("no audio source attached to money");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void StepOneEvent() 
+    {
+        Debug.Log("step1");
+    }
+
+    public void SteoTwoEvent() 
+    {
+        Debug.Log("step2");
+    }
+
+    public void ArrivedPoint() 
+    {
+
     }
 }
